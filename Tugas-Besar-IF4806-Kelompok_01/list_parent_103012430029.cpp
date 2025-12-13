eb53e4c975578a9714d318207c45f6b5369c61a4
rhmtulalim_ <rahmatulalim@gmail.com>
2025-12-13 10:54:58 +0700

    Rename file dan tambah file baru

--- Tugas-Besar-IF4806-Kelompok_01/list_parent_103012430029.cpp
+++ Tugas-Besar-IF4806-Kelompok_01/list_parent_103012430029.cpp
@@ -1,43 +1,5 @@
 #include "list_parent.h"
 
-void createListPaper(ListPaper &L) {
-    L.first = nullptr;
-}
-
-addressPaper alokasiPaper(infotypePaper x) {
-    addressPaper P = new elmenPaper;
-    P->info = x;
-    P->next = nullptr;
-    return P;
-}
-
-void insertFirstPaper(ListPaper &L, addressPaper P) {
-    P->next = L.first;
-    L.first = P;
-}
-
-void insertLastPaper(ListPaper &L, addressPaper P) {
-    if (L.first == nullptr) {
-        insertFirstPaper(L, P);
-    } else {
-        addressPaper Q = L.first;
-        while (Q->next != nullptr) {
-            Q = Q->next;
-        }
-        Q->next = P;
-    }
-}
-
-void insertAfterPaper(ListPaper &L, addressPaper Prec, addressPaper P) {
-    if (P == nullptr) return;
-    if (Prec == nullptr) {
-        insertFirstPaper(L, P);
-    } else {
-        P->next = Prec->next;
-        Prec->next = P;
-    }
-}
-
 void deleteFirstPaper(ListPaper &L, addressPaper &P) {
     if (L.first != nullptr) {
         P = L.first;
