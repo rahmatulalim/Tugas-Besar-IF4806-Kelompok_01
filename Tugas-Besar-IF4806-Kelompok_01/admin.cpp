24e005cae72ac30b7e82fcfe2d8738495b614d5c
Rahmatul Akbar Alim <rahmatulalim@gmail.com>
2025-12-20 10:19:15 +0700

    Refactor adminMenu function and improve code structure

--- Tugas-Besar-IF4806-Kelompok_01/admin.cpp
+++ Tugas-Besar-IF4806-Kelompok_01/admin.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "admin.h"
 
 using namespace std;
 
-void adminMenu(ListPaper &LP, ListKeyword &LK, ListRelasi &LR) {
-    int pilihan = -1;   // WAJIB diinisialisasi
+void adminMenu(ListPaper &LP, ListKeyword &LK) {
+    int pilihan = -1;
     string idPaper, judulPaper, namaKeyword, targetID, targetKW;
     int tahunPaper;
     addressPaper P, PrecP;
     addressKeyword K, PrecK;
 
     while (pilihan != 0) {
+        system("cls");
         cout << "===========================================" << endl;
         cout << "          ADMIN DASHBOARD (PRIMITIF)       " << endl;
         cout << "===========================================" << endl;
@@ -34,123 +36,95 @@ void adminMenu(ListPaper &LP, ListKeyword &LK, ListRelasi &LR) {
         cin >> pilihan;
 
         switch (pilihan) {
-
-        // ===== PAPER =====
         case 1:
             cout << "ID    : "; cin >> idPaper;
             cout << "Judul : "; cin >> judulPaper;
             cout << "Tahun : "; cin >> tahunPaper;
             insertFirstPaper(LP, alokasiPaper({judulPaper, idPaper, tahunPaper}));
+            system("pause");
             break;
-
         case 2:
             cout << "ID    : "; cin >> idPaper;
             cout << "Judul : "; cin >> judulPaper;
             cout << "Tahun : "; cin >> tahunPaper;
             insertLastPaper(LP, alokasiPaper({judulPaper, idPaper, tahunPaper}));
+            system("pause");
             break;
-
         case 3:
-            cout << "Cari ID Paper sebelumnya: ";
-            cin >> targetID;
+            cout << "Cari ID Paper sebelumnya: "; cin >> targetID;
             PrecP = findPaper(LP, targetID);
             if (PrecP != nullptr) {
                 cout << "ID Baru    : "; cin >> idPaper;
                 cout << "Judul Baru : "; cin >> judulPaper;
                 cout << "Tahun      : "; cin >> tahunPaper;
-                insertAfterPaper(LP, PrecP,
-                    alokasiPaper({judulPaper, idPaper, tahunPaper}));
-            } else {
-                cout << "ID tidak ditemukan!" << endl;
-            }
+                insertAfterPaper(LP, PrecP, alokasiPaper({judulPaper, idPaper, tahunPaper}));
+            } else cout << "ID tidak ditemukan!" << endl;
+            system("pause");
             break;
-
         case 4:
             deleteFirstPaper(LP, P);
-            if (P != nullptr) {
-                removeRelasiByPaper(LR, P->info.idPaper);
-                delete P;
-            }
+            cout << ">> Berhasil dihapus." << endl;
+            system("pause");
             break;
-
         case 5:
             deleteLastPaper(LP, P);
-            if (P != nullptr) {
-                removeRelasiByPaper(LR, P->info.idPaper);
-                delete P;
-            }
+            cout << ">> Berhasil dihapus." << endl;
+            system("pause");
             break;
-
         case 6:
-            cout << "Cari ID Paper sebelumnya: ";
-            cin >> targetID;
+            cout << "Cari ID Paper sebelumnya: "; cin >> targetID;
             PrecP = findPaper(LP, targetID);
             deleteAfterPaper(LP, PrecP, P);
-            if (P != nullptr) {
-                removeRelasiByPaper(LR, P->info.idPaper);
-                delete P;
-            }
+            cout << ">> Berhasil dihapus." << endl;
+            system("pause");
             break;
-
-        // ===== KEYWORD (TANPA ID) =====
         case 7:
-            cout << "Keyword: ";
-            cin >> namaKeyword;
+            cout << "Keyword: "; cin >> namaKeyword;
             insertFirstKeyword(LK, alokasiKeyword({namaKeyword}));
+            system("pause");
             break;
-
         case 8:
-            cout << "Keyword: ";
-            cin >> namaKeyword;
+            cout << "Keyword: "; cin >> namaKeyword;
             insertLastKeyword(LK, alokasiKeyword({namaKeyword}));
+            system("pause");
             break;
-
         case 9:
-            cout << "Cari Keyword sebelumnya: ";
-            cin >> targetKW;
+            cout << "Cari Keyword sebelumnya: "; cin >> targetKW;
             PrecK = findKeyword(LK, targetKW);
             if (PrecK != nullptr) {
-                cout << "Keyword Baru: ";
-                cin >> namaKeyword;
+                cout << "Keyword Baru: "; cin >> namaKeyword;
                 insertAfterKeyword(LK, PrecK, alokasiKeyword({namaKeyword}));
-            } else {
-                cout << "Keyword tidak ditemukan!" << endl;
-            }
+            } else cout << "Keyword tidak ditemukan!" << endl;
+            system("pause");
             break;
-
         case 10:
             deleteFirstKeyword(LK, K);
             if (K != nullptr) {
-                removeRelasiByKeyword(LR, K->info.namaKeyword);
-                delete K;
+                addressPaper p = LP.first;
+                while (p != nullptr) { disconnectKeywordFromPaper(p, K->info.namaKeyword); p = p->next; }
             }
+            system("pause");
             break;
-
         case 11:
             deleteLastKeyword(LK, K);
             if (K != nullptr) {
-                removeRelasiByKeyword(LR, K->info.namaKeyword);
-                delete K;
+                addressPaper p = LP.first;
+                while (p != nullptr) { disconnectKeywordFromPaper(p, K->info.namaKeyword); p = p->next; }
             }
+            system("pause");
             break;
-
         case 12:
-            cout << "Cari Keyword sebelumnya: ";
-            cin >> targetKW;
+            cout << "Cari Keyword sebelumnya: "; cin >> targetKW;
             PrecK = findKeyword(LK, targetKW);
             deleteAfterKeyword(LK, PrecK, K);
             if (K != nullptr) {
-                removeRelasiByKeyword(LR, K->info.namaKeyword);
-                delete K;
+                addressPaper p = LP.first;
+                while (p != nullptr) { disconnectKeywordFromPaper(p, K->info.namaKeyword); p = p->next; }
             }
+            system("pause");
             break;
-
-        case 0:
-            cout << "Kembali ke menu utama..." << endl;
-            break;
-
-        default:
-            cout << "Pilihan tidak valid." << endl;
+        case 0: break;
         }
     }
 }
+
