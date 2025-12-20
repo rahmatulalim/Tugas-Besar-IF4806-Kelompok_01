a174095b8963490ef7d5c34bebb519f4b87f834b
Rahmatul Akbar Alim <rahmatulalim@gmail.com>
2025-12-20 10:19:53 +0700

    Remove ListRelasi from adminMenu parameters

--- Tugas-Besar-IF4806-Kelompok_01/admin.h
+++ Tugas-Besar-IF4806-Kelompok_01/admin.h
@@ -3,8 +3,8 @@
 
 #include "list_parent.h"
 #include "list_child.h"
-#include "list_relasi.h"
 
-void adminMenu(ListPaper &LP, ListKeyword &LK, ListRelasi &LR);
+void adminMenu(ListPaper &LP, ListKeyword &LK);
 
 #endif
+
