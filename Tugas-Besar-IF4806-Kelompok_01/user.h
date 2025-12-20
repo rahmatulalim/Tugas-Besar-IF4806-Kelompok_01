66e2da9e0762db9c641fdb5f641e7c075867647f
Rahmatul Akbar Alim <rahmatulalim@gmail.com>
2025-12-20 10:25:58 +0700

    Remove ListRelasi from userMenu parameters

--- Tugas-Besar-IF4806-Kelompok_01/user.h
+++ Tugas-Besar-IF4806-Kelompok_01/user.h
@@ -3,8 +3,8 @@
 
 #include "list_parent.h"
 #include "list_child.h"
-#include "list_relasi.h"
 
-void userMenu(ListPaper &LP, ListKeyword &LK, ListRelasi &LR);
+void userMenu(ListPaper &LP, ListKeyword &LK);
 
 #endif
+
