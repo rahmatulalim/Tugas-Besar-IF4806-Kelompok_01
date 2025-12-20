1658636e259cfa717d1f689b428a935211f447d7
Rahmatul Akbar Alim <rahmatulalim@gmail.com>
2025-12-20 10:24:53 +0700

    Update main.cpp

--- Tugas-Besar-IF4806-Kelompok_01/main.cpp
+++ Tugas-Besar-IF4806-Kelompok_01/main.cpp
@@ -1,69 +1,36 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "list_child.h"
 #include "list_parent.h"
-#include "list_relasi.h"
 #include "user.h"
 #include "admin.h"
 
 using namespace std;
 
-void showMenu() {
-    cout << "========================================" << endl;
-    cout << "   APLIKASI PENGELOLA PAPER & KEYWORD   " << endl;
-    cout << "========================================" << endl;
-    cout << "1. Masuk sebagai USER" << endl;
-    cout << "2. Masuk sebagai ADMIN" << endl;
-    cout << "0. Keluar" << endl;
-    cout << "========================================" << endl;
-    cout << "Pilihan Anda: ";
-}
-
 int main() {
     ListPaper LP;
     ListKeyword LK;
-    ListRelasi LR;
-
     createListPaper(LP);
     createListKeyword(LK);
-    createListRelasi(LR);
 
-    int pilihan;
+    int pilihan = -1;
     do {
-        showMenu();
+        system("cls");
+        cout << "========================================" << endl;
+        cout << "   APLIKASI PENGELOLA PAPER & KEYWORD   " << endl;
+        cout << "========================================" << endl;
+        cout << "1. Masuk sebagai USER" << endl;
+        cout << "2. Masuk sebagai ADMIN" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "========================================" << endl;
+        cout << "Pilihan: ";
         cin >> pilihan;
-        switch (pilihan) {
-            case 1:
-                userMenu(LP, LK, LR);
-                break;
-            case 2:
-                adminMenu(LP, LK, LR);
-                break;
-            case 0:
-                cout << "Terima kasih. Program selesai." << endl;
-                break;
-            default:
-                cout << "Pilihan tidak valid." << endl;
-        }
+
+        if (pilihan == 1) userMenu(LP, LK);
+        else if (pilihan == 2) adminMenu(LP, LK);
     } while (pilihan != 0);
-
-    while (LR.first != nullptr) {
-        addressRelasi r = LR.first;
-        LR.first = r->next;
-        delete r;
-    }
-
-    addressPaper ap;
-    while (LP.first != nullptr) {
-        deleteFirstPaper(LP, ap);
-        if (ap != nullptr) delete ap;
-    }
-
-    addressKeyword ak;
-    while (LK.first != nullptr) {
-        deleteFirstKeyword(LK, ak);
-        if (ak != nullptr) delete ak;
-    }
 
     return 0;
 }
+
